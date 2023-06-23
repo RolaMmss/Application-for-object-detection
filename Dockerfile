51dd25934e6e61a46ed751eb1256fcaacfa961bd
RolaMmss <rola.el-moallem@hotmail.fr>
2023-06-23 11:28:02 +0200

    model mod

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,19 @@
-
+#Cette ligne spécifie l'image de base pour le conteneur Docker. Dans ce cas, elle utilise l'image python:3.11-slim, qui fournit un environnement Python 3.11 minimal.
 FROM python:3.11-slim
 
+# Cela définit le répertoire de travail à l'intérieur du conteneur sur /streamlit.
 WORKDIR /streamlit
 
+# Cela définit la variable d'environnement PYTHONUNBUFFERED à 1, ce qui garantit que la sortie de Python n'est pas mise en mémoire tampon, permettant un affichage en temps réel des journaux.
 ENV PYTHONUNBUFFERED 1
 
+# Cela copie le fichier requirements.txt de l'hôte vers le répertoire /streamlit à l'intérieur du conteneur.
 COPY requirements.txt /streamlit
 
-RUN pip install --upgrade pip && \ 
-    pip install --no-cache-dir -r requirements.txt
-
-COPY yolov8n.pt /streamlit
-
-COPY . /streamlit.py
+# installe les dépendances requises spécifiées dans requirements.txt 
+RUN  pip install -r requirements.txt
+    # ls
+COPY . /streamlit
 
 EXPOSE 8501
 
--- streamlit.py
+++ streamlit.py
@@ -9,42 +9,36 @@ from PIL import Image, ImageDraw
 from ultralytics import YOLO
 import numpy as np
 
-model = YOLO("yolov8n.yaml")
-model.load("yolov8n.pt")
+model = YOLO("best.pt")
+# model=YOLO("yolov8n.pt")
 
 # # Class labels for car makes
 # class_labels = ['Audi', 'BMW', 'Mercedes', 'MercedesBenz', 'Toyota', 'Volkswagen', 'toyota']
 
+# metrics = model.val() 
+# print("Precision: ", metrics.results_dict['metrics/precision(B)'])
+# print("Recall: ", metrics.results_dict['metrics/recall(B)'])
+# print("mAP: ", metrics.results_dict['metrics/mAP50(B)'])
+# print("Fitness: ", metrics.fitness)
 
 def main():
     st.title("Prédire la marque de voiture!")
     st.write("Upload an image or a video file to predict the car make.")
 
     # Add file uploader
-    file = st.file_uploader("Upload Image or Video", type=["jpg", "jpeg", "png", "mp4"])
+    file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
 
     if file is not None:
         if file.type.startswith('image'):
             # Process uploaded image
             img = Image.open(file)
             st.image(img, caption='Uploaded Image', use_column_width=True)
-            results= model(img)
-            # results = model.predict(img, conf=0.25)
-
-           # Display predicted labels on the image
-            # annotated_img = results.render()  # Render annotated image with bounding boxes and labels
-            # annotated_img = Image.fromarray(annotated_img)  # Convert the image array to PIL Image
-            # st.image(img, caption='Predicted Image', use_column_width=True)
-            
+            results= model(img)  
         
-            # # Plot the results on the image
+            # Plot the results on the image
             res_plotted = results[0].plot(labels=True)
             st.image(res_plotted, caption='Predicted Image', use_column_width=True)
             
-            # Display the predicted image
-            # st.image(img, caption='Predicted Image', use_column_width=True)
-            # res=results[0].plot()
-            # st.image(res)
         elif file.type.startswith('video'):
             # Process uploaded video
             video = cv2.VideoCapture(file)
